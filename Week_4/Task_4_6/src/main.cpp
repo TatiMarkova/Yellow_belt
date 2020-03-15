95e474d78ef9e6f9e1c93b5cec6242b57fc50fde
Tati Markova <tatiana.e.markova@gmail.com>
2020-03-15 19:16:47 +0100

    Task_4_6

---
+++ Week_4/Task_4_6/src/main.cpp
@@ -0,0 +1,52 @@
+/****************************
+ * Coursera                 *
+ * Yellow belt              *
+ * @autor: Tatiana Markova  *
+ * task: 4_6                *
+ ****************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+/********************
+ * Input:  (main)   *
+ *   3              *
+ ********************/
+
+template<typename T>
+void RemoveDuplicates(vector<T>& elements) {
+    sort(rbegin(elements), rend(elements));
+    auto it = unique(begin(elements), end(elements));
+    elements.erase(it, end(elements));
+} 
+
+int main(int argv, char* argc[]) {
+    int count;
+    cin >> count;
+    vector<int> nums(count);
+    iota(rbegin(nums), rend(nums), 1);
+    do {
+        for (auto& num : nums) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+    } while (prev_permutation(begin(nums), end(nums)));
+    cout << '\n';
+
+    return EXIT_SUCCESS;
+}
+
+/********************
+ * Output:          *
+ *   3 2 1          *
+ *   3 1 2          *
+ *   2 3 1          *
+ *   2 1 3          *
+ *   1 3 2          *
+ *   1 2 3          *
+ ********************/
